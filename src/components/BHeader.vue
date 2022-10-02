023c98f460604d74138045e482dca2cf363cdd75
Lucas Duarte <lucs.rkt@gmail.com>
2022-10-02 13:56:02 -0300

    feat: create header and navbar components

---
+++ src/components/BHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <div>Logo do Billinho</div>
+    <BNavBarVue />
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import BNavBarVue from "./BNavBar.vue";
+
+export default defineComponent({
+  name: "BHeader",
+  components: {
+    BNavBarVue,
+  },
+});
+</script>
---
+++ src/components/BNavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <ul>
+      <router-link to="/institutions">Instituições</router-link>
+      <router-link to="/students">Alunos</router-link>
+      <router-link to="/enrollments">Matrículas</router-link>
+    </ul>
+    <div>Avatar</div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BNavBar",
+});
+</script>
