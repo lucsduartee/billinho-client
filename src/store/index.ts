595f9a22b94bf08bd95528ac457a22b2d82010fd
Lucas Duarte <lucs.rkt@gmail.com>
2022-10-07 14:10:36 -0300

    feat: create global store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,45 @@
-import { createStore } from "vuex";
+import IInstitutions from "@/interfaces/IInstitutions";
+import Resources from "@/utils/resources";
+import { InjectionKey } from "vue";
+import { createStore, Store, useStore as vuexUseStore } from "vuex";
+import { FETCH_INSTITUTIONS } from "./actionsTypes";
+import httpClient from "@/http";
+import { ADD_INSTITUTIONS } from "./mutationsTypes";
+import { INSTITUTIONS } from "./gettersTypes";
 
-export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+interface State {
+  institutions: Array<IInstitutions>;
+}
+
+const store = createStore<State>({
+  state: {
+    institutions: [],
+  },
+  getters: {
+    [INSTITUTIONS](state) {
+      state.institutions;
+    },
+  },
+  mutations: {
+    [ADD_INSTITUTIONS](state, institutions: IInstitutions[]) {
+      state.institutions = institutions;
+    },
+  },
+  actions: {
+    async [FETCH_INSTITUTIONS]({ commit }) {
+      const institutions = await httpClient.get(Resources.INSTITUTIONS);
+      const { data } = institutions.data;
+
+      commit(ADD_INSTITUTIONS, data);
+    },
+  },
   modules: {},
 });
+
+export const key: InjectionKey<Store<State>> = Symbol();
+
+export function useStore(): Store<State> {
+  return vuexUseStore(key);
+}
+
+export default store;
