cc41ecaa5f7d2f6e6e914f4e80da9b38eabf4b84
Lucas Duarte <lucs.rkt@gmail.com>
2022-10-08 12:57:48 -0300

    :sparkles: create table components

---
+++ src/components/BTableBody.vue
@@ -0,0 +1,23 @@
+<template>
+  <tbody>
+    <tr v-for="(item, index) in content" :key="index">
+      <td v-for="(value, index) in item" :key="index">
+        {{ value }}
+      </td>
+    </tr>
+  </tbody>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BTableBody",
+  props: {
+    content: {
+      type: Array,
+      required: true,
+    },
+  },
+});
+</script>
---
+++ src/components/BTableHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <thead>
+    <tr>
+      <th v-for="(column, index) in columnNames" :key="index">{{ column }}</th>
+      <th>Ações</th>
+    </tr>
+  </thead>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BTableHeader",
+  props: {
+    columnNames: {
+      type: Array,
+      required: true,
+    },
+  },
+});
+</script>
